a17cadc76bd881477b2c983357623ef87db87ab4
alkaz07 <alkaz07@yandex.ru>
2024-10-01 16:53:08 +0300

    +фильтрация списка

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Сделать список из нескольких роботов с  разными исходными координатами и направлениями
+        List<Robot> robots = new LinkedList<>();
+        robots.add(new Robot(1, 2, Directions.North));
+        robots.add(new Robot(22, 3, Directions.North));
+        robots.add(new Robot(4, 44, Directions.North));
+        robots.add(new Robot(0, 12, Directions.East));
+        robots.add(new Robot(3, 22, Directions.West));
+        robots.add(new Robot(200, 2, Directions.South));
+        robots.add(new Robot(100, 12, Directions.South));
+        //Отфильтровать в новый список тех кто смотрит на Юг
+       /* List<Robot> southWatchers = robots.stream()
+                                        .filter(rob -> rob.direction == Directions.South)
+                                        .toList();*/
+        List<Robot> southWatchers = new LinkedList<>();
+        for (Robot rob: robots)
+            if(rob.direction  == Directions.South)
+                southWatchers.add(rob);
+
+        System.out.println("southWatchers = " + southWatchers);
     }
 }
 
@@ -11,11 +33,19 @@ enum Directions{
 class Robot{
     int x, y;
     Directions direction;
-
     public Robot(int x, int y, Directions direction) {
         this.x = x;
         this.y = y;
         this.direction = direction;
+    }
+
+    @Override
+    public String toString() {
+        return "Robot{" +
+                "x=" + x +
+                ", y=" + y +
+                ", direction=" + direction +
+                '}';
     }
 
     public void turnLeft(){
@@ -26,7 +56,6 @@ class Robot{
             case Directions.West:  direction = Directions.South;    break;
         }
     }
-
     public void turnRight(){
         switch (direction){
             case Directions.East : direction = Directions.South;    break;
@@ -35,6 +64,4 @@ class Robot{
             case Directions.West:  direction = Directions.North;    break;
         }
     }
-
-
 }
