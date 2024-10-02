b163afe4f5a64d10da1d32d38141b859b51538cd
alkaz07 <alkaz07@yandex.ru>
2024-10-02 10:55:15 +0300

    +подсчет количеств через Map +загрузка из файла

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-import java.util.LinkedList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class Main {
@@ -23,8 +24,40 @@ public class Main {
                 southWatchers.add(rob);
 
         System.out.println("southWatchers = " + southWatchers);
+
+        //Посчитать, сколько роботов смотрят в каждом направлении
+        Map<Directions, Integer> directionCount = new HashMap<>();
+        for (Robot rob: robots){
+            Directions dir = rob.direction;
+            int count = directionCount.getOrDefault(dir, 0);
+            directionCount.put(dir, count+1);
+        }
+
+        System.out.println("directionCount = " + directionCount);
+        
+        List<Robot> robots2 = loadRobots("input");
+        System.out.println("robots2 = " + robots2);
+    }
+
+    public static List<Robot> loadRobots(String filePath) {
+        List<Robot> robots = new LinkedList<>();
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNext()) {
+                String line = scanner.nextLine();
+                String[] parts = line.split(",");
+                int x = Integer.parseInt(parts[0].trim());
+                int y = Integer.parseInt(parts[1].trim());
+                Directions direction = Directions.valueOf(parts[2].trim());
+                robots.add(new Robot(x, y, direction));
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Файл не найден: " + e.getMessage());
+        } return robots;
     }
 }
+
+
+
 
 enum Directions{
     North, East, South, West;
